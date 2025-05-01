ce8ef875718da17cdc791a23d53a7798973c7323
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-05-01 07:37:26 -0500

    feat(web): styling

--- job-hop-web/src/app/page.tsx
+++ job-hop-web/src/app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 import { Box, Typography, Paper, Link } from '@mui/material';
 import Image from 'next/image';
+import { useTheme } from '@mui/material/styles';
 
 export default function Home() {
+  const theme = useTheme();
   return (
     <>
       <main>
-        <Box minHeight="100vh" display="flex" alignItems="center" justifyContent="center" bgcolor="#111827">
-          <Paper elevation={3} sx={{ bgcolor: '#1f2937', p: 8, borderRadius: 2, width: '100%', maxWidth: 400, mx: 'auto', my: 6 }}>
-            <Typography variant="h4" fontWeight="bold" textAlign="center" color="#f3f4f6" mb={2}>
+        <Box minHeight="100vh" display="flex" alignItems="center" justifyContent="center" bgcolor={theme.palette.background.default}>
+          <Paper elevation={3} sx={{ bgcolor: theme.palette.background.paper, p: 8, borderRadius: 2, width: '100%', maxWidth: 400, mx: 'auto', my: 6 }}>
+            <Typography variant="h4" fontWeight="bold" textAlign="center" color={theme.palette.text.primary} mb={2}>
               Welcome to Job Hop
             </Typography>
-            <Typography color="#d1d5db" textAlign="center" mb={3}>
+            <Typography color={theme.palette.text.secondary} textAlign="center" mb={3}>
               Your job search starts here!
             </Typography>
             <Image src="/job_hop_logo.png" alt="Job Hop Logo" width={150} height={150} style={{ display: 'block', margin: '0 auto' }} />
